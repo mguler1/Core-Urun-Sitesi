e2bc41b02ac334e858ecfcdee073cb81f0a84166
= <=>
2021-03-03 17:38:58 +0300

    Layout düzenlemeleri yapıldı

---
+++ CoreAndFood/ViewComponents/CategoryGet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreAndFood.Repositories;
+using Microsoft.AspNetCore.Mvc;
+namespace CoreAndFood.ViewComponents
+{
+    public class CategoryGet : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            CategoriRepository categoriRepository = new CategoriRepository();
+            var categoryList = categoriRepository.TList();
+            return View(categoryList);
+        }
+    }
+}
